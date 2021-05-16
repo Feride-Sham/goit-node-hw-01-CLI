3354d6a751ded189191e9e9c91590866c5c27bc9
Feride Shamsutdinova <ferika.sham@gmail.com>
2021-05-16 23:56:33 +0300

    feat: add  import in index.js

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-console.log("hello");
+const contacts = require("./contacts.js");
+
+// console.log(contacts.listContacts());
+console.log(
+  contacts.addContact("Allen Raymond", "lila@mail.ru", "+789165789416")
+);
